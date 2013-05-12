33c9d7c35695ae323f59fdd0010cbee73ed13d18
John Fischer <me@jfroffice.me>
2013-05-12 16:02:32 +0200

    disqus now working

--- app.js
+++ app.js
@@ -3,19 +3,19 @@
  */
 
 var express = require('express'),
-	http = require('http'),
-	livereload = require('express-livereload'),
-	md = require('./service/md'),
-	path = require('path');
+    http = require('http'),
+    livereload = require('express-livereload'),
+    md = require('./service/md'),
+    path = require('path');
 
 var app = express();
 
 livereload(app, {
-	watchDir: __dirname + '/public'
+    watchDir: __dirname + '/public'
 });
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 6001);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -24,11 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/md', md.list);
 
 http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
