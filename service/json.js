22f5da93a273ad42a53d175e1c94386ba52b4406
jfroffice <me@jfroffice.me>
2013-05-19 16:19:34 +0200

    add json method parsing metadata files

--- service/json.js
+++ service/json.js
@@ -1,14 +1,16 @@
 var path = require("path"),
-	readdirp = require('readdirp');
+	readdirp = require('readdirp'),
+	root = path.join(__dirname, '../public/md');
 
 exports.list = function(req, res) {
 	var files = [];
 	readdirp({
-		root: path.join(__dirname, '../public/md'),
+		root: root,
 		fileFilter: '*.json'
 	}).on('data', function(data) {
 		files.push({
-			name: data.name
+			name: data.name.replace('.json', '.md'),
+			data: require(path.join(root, data.name))
 		});
 	}).on('end', function() {
 		res.send({
